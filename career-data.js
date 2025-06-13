beaaf6a3171cceeac31e5beee5de73e98578ade8
sami-khatib1 <86168771+sami-khatib1@users.noreply.github.com>
2025-06-13 15:26:40 +0300

    Create career-data.js

---
+++ career-data.js
@@ -0,0 +1,22 @@
+const careers = [
+  {
+    title: "קופאי/ת",
+    image: "careers/career_cashier.jpeg",
+    description: "עבודה עם לקוחות בקופה, ניהול תשלומים ומתן שירות איכותי."
+  },
+  {
+    title: "עובד/ת סידור סחורה",
+    image: "careers/career_shelf.jpeg",
+    description: "אחריות על סידור מדפים, מלאי ונראות הסחורה."
+  },
+  {
+    title: "קצב/ית",
+    image: "careers/career_buch.jpeg",
+    description: "עבודה בדלפק הבשר, הכנת הזמנות, שמירה על טריות וניקיון."
+  },
+  {
+    title: "ירקן/ית",
+    image: "careers/career_veg.jpeg",
+    description: "שמירה על ניקיון ותחזוקה שוטפת של הסופרמרקט."
+  },
+];
